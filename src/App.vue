598d603659367361b89c6ae4604d23cef26965fe
amihhs <amihhs369@gmail.com>
2022-11-15 14:08:37 +0800

    init

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<script setup>
+import {NTree} from 'naive-ui'
+import {ref} from 'vue'
+const options = ref([{
+          label: 'l-0',
+          key: 'v-0',
+          depth: 1,
+          isLeaf: false
+        }])
+async function loadHandle(node) {
+  console.log('loadHandle')
+    return false
+}
+</script>
+
+<template>
+  <div>
+    <n-tree :data="options"  @load="loadHandle"></n-tree>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
